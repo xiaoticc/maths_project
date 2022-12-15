7473683c650981def286ca5ec29dd25154be602e
Nikitin Rat <rote808out@gmail.com>
2022-12-15 18:50:50 +0300

    upd_project

--- prof.py
+++ prof.py
@@ -1,7 +1,11 @@
 import sys
-
+import sqlite3
+from PyQt5.QtCore import Qt, QSize
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QScrollArea
+from PyQt5.QtWidgets import QApplication, QMainWindow, QScrollArea, QGroupBox, QLabel, QLineEdit, QVBoxLayout, \
+    QPushButton
+
+con = sqlite3.connect("maths_db.sqlite")
 
 
 class Main(QMainWindow):
@@ -48,13 +52,62 @@ class Test(QMainWindow):
         uic.loadUi('test.ui', self)
         # подгружаем дизайн
         self.btn_endt.clicked.connect(self.open_res)
+        self.set_temp_task()
+
+    def set_temp_task(self):
+        cur = con.cursor()
+
+        self.data = cur.execute("""SELECT * FROM task_answer, tasks_theme
+                                    WHERE theme_id = task_id AND
+                                    temp_answer = 1""").fetchall()
+        print(self.data)
+        self.layout = QVBoxLayout()
+
+        for i, el in enumerate(self.data):
+            self.layout.addWidget(self.create_ui_answer(i + 1, el[2]))
+            print(el)
+        self.scrollArea.setWidgetResizable(True)
+        # self.scrollArea.setLayout(layout)
+        self.scrollAreaWidgetContents_2.setLayout(self.layout)
+        self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+    def create_ui_answer(self, num, name):
+        layout = QVBoxLayout()
+        groupBox = QGroupBox(f"Вопрос {num}")
+        groupBox.setMinimumHeight(200)
+        groupBox.setMinimumWidth(300)
+        label = QLabel(name, self)
+        label.setWordWrap(True)
+        line_edit = QLineEdit(self)
+        layout.addWidget(label)
+        layout.addWidget(line_edit)
+        groupBox.setLayout(layout)
+        return groupBox
 
     def open_res(self):
         # открытие результатов теста
-        self.res_window = Results()
+
+        self.res_window = Results(*self.results())
         self.res_window.show()
         self.res_window.move(self.pos())
         self.hide()
+
+    def results(self):
+        cur = con.cursor()
+        counter_right = 0
+        wrong_theme = [0, 0, 0]
+        for i, el in enumerate(self.data):
+            box = self.layout.itemAt(i).widget()
+            line = box.findChildren(QLineEdit)[0]
+            user_answer = line.text()
+            right_answer = str(el[3])
+            if user_answer == right_answer:
+                counter_right += 1
+            else:
+                wrong_theme[int(el[1]) - 1] += 1
+        return counter_right, wrong_theme
+        # print(self.layout.itemAt(0).widget().findChildren(QLineEdit)[0].text())
 
 
 class Profs(QMainWindow):
@@ -68,7 +121,8 @@ class Profs(QMainWindow):
 
     def open_journals(self):
         # открытие статей
-        self.journ_window = Journals()
+        text = self.sender().text()
+        self.journ_window = Journals(text)
         self.journ_window.show()
         self.journ_window.move(self.pos())
         self.hide()
@@ -83,20 +137,71 @@ class Profs(QMainWindow):
 
 class Results(Profs):
     # наследуется от класса Profs, тк у них действует один и тот же метод возвращения в главное меню
-    def __init__(self, pos_wnd=None):
+    def __init__(self, right, wrong_theme, pos_wnd=None):
         super().__init__()
         uic.loadUi('results.ui', self)
         # подгружаем дизайн
+        self.right = right
+        self.wrong_theme = wrong_theme
         self.pos_wnd = pos_wnd
         self.btn_mainr.clicked.connect(self.back_to_main)
+        self.create_answer()
+
+    def create_answer(self):
+        cur = con.cursor()
+        themes = cur.execute("""SELECT task_var FROM tasks_theme""").fetchall()
+        themes = [el[0] for el in themes]
+        a = '\n'.join([themes[el] for el in range(len(self.wrong_theme)) if self.wrong_theme[el] != 0])
+        self.label_2.setText(f"""количество правильных ответов: {self.right} 
+количество неправильных ответов: {sum(self.wrong_theme)}
+у вас проблемы с темой(-ами): {a}""")
 
 
 class Journals(QMainWindow):
-    def __init__(self):
+    def __init__(self, name):
         super(Journals, self).__init__()
         uic.loadUi('journals.ui', self)
         # подгружаем дизайн
+        self.name = name
         self.btn_return.clicked.connect(self.back_to_profs)
+        self.set_task()
+        for el in self.layout.itemAt(0).widget().findChildren(QLineEdit):
+            print(1)
+
+    def set_task(self):
+        cur = con.cursor()
+        task = self.name
+        self.data = cur.execute("""SELECT * FROM task_answer, tasks_theme
+                                    WHERE theme_id = task_id AND
+                                    temp_answer = 0 AND
+                                    task_var =:task""", {"task": task}).fetchall()
+        print(self.data)
+        self.layout = QVBoxLayout()
+
+        for i, el in enumerate(self.data):
+            self.layout.addWidget(self.create_ui_answer(i + 1, el[2]))
+        self.scrollArea.setWidgetResizable(True)
+        # self.scrollArea.setLayout(layout)
+        self.scrollAreaWidgetContents.setLayout(self.layout)
+        self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+    def create_ui_answer(self, num, name):
+        layout = QVBoxLayout()
+        groupBox = QGroupBox(f"Вопрос {num}")
+        print(num)
+        self.label.setText(f'Тренажер на тему: {self.name}')
+        groupBox.setMinimumHeight(200)
+        groupBox.setMinimumWidth(300)
+        label = QLabel(name, self)
+        label.setWordWrap(True)
+        line_edit = QLineEdit(self)
+        # событие на изменение line_edit
+        line_edit.textChanged.connect(lambda x: self.doSomething(num - 1))
+        layout.addWidget(label)
+        layout.addWidget(line_edit)
+        groupBox.setLayout(layout)
+        return groupBox
 
     def back_to_profs(self):
         # возвращает обратно к списку профессий
@@ -104,6 +209,15 @@ class Journals(QMainWindow):
         self.profs_wnd.show()
         self.profs_wnd.move(self.pos())
         self.close()
+
+    def doSomething(self, num_question):
+        user_answer = self.sender().text()
+        right_answer = str(self.data[num_question][3])
+        print(right_answer, user_answer, self.data[num_question][3])
+        if user_answer == right_answer:
+            self.sender().setStyleSheet("color: rgb(0, 255, 0);")
+        else:
+            self.sender().setStyleSheet("color: rgb(255, 0, 0);")
 
 
 # нужная вещь для отображения ошибок не кодами возврата
